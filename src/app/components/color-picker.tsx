0d04bd84599e0408ffd34fd544751f708aaa9f83
harvey <harveytseng2@gmail.com>
2025-03-18 22:59:25 -0400

    prettifies color pickers

--- src/app/components/color-picker.tsx
+++ src/app/components/color-picker.tsx
@@ -1,19 +1,20 @@
 interface ColorPickerProps {
-  color: string;
-  setColor: (color: string) => void;
-}
-
-export default function ColorPicker({ color, setColor }: ColorPickerProps) {
-  return (
-    <div className="flex flex-col items-center gap-4 w-full">
-      <label htmlFor="color-picker">Pick a color:</label>
-      <input
-        id="color-picker"
-        type="color"
-        value={color}
-        onChange={(e) => setColor(e.target.value)}
-        className="w-full rounded-full"
-      />
-    </div>
-  );
-}
+    color: string;
+    setColor: (color: string) => void;
+  }
+  
+  export default function ColorPicker({ color, setColor }: ColorPickerProps) {
+    return (
+      <div className="flex flex-col items-center gap-4 w-full">
+        <label htmlFor="color-picker" className="text-lg text-center">Pick a color:</label>
+        <input
+          id="color-picker"
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          className="w-full max-w-xs h-8 sm:h-12 cursor-pointer rounded"
+        />
+        <div className="text-sm font-mono">{color}</div>
+      </div>
+    );
+  }
--- src/app/components/color-preset.tsx
+++ src/app/components/color-preset.tsx
@@ -4,11 +4,11 @@ interface ColorPresetProps {
 }
 
 export default function ColorPreset({ color, setColor }: ColorPresetProps) {
-return (
+  return (
     <button
-        className="flex items-center justify-center p-4 sm:p-6 md:p-8 border-2 border-white rounded-full"
-        style={{ backgroundColor: color }}
-        onClick={() => setColor(color)}
+      className="flex items-center justify-center p-4 sm:p-6 md:p-8 border-2 border-white rounded-full"
+      style={{ backgroundColor: color }}
+      onClick={() => setColor(color)}
     />
-);
+  );
 }
