ce0ad4d47c5865afb478ec398dd9efa670dceb01
harvey <harveytseng2@gmail.com>
2025-03-18 22:42:35 -0400

    creates avatar component

---
+++ src/app/components/avatar.tsx
@@ -0,0 +1,10 @@
+export default function Avatar({ color }: { color: string }) {
+  return (
+    <div
+      className="flex items-center justify-center p-6 sm:p-24 border-4 border-white transition-colors duration-700"
+      style={{ backgroundColor: color }}
+    >
+      <span className="inline-block w-16 h-16 sm:w-32 sm:h-32 md:w-64 md:h-64 bg-white border-4 border-black rounded-full" />
+    </div>
+  );
+}
