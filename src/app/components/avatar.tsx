d04b234d90cfe9d6c2e902983f695a9b54323ba6
harvey <harveytseng2@gmail.com>
2025-03-18 22:59:11 -0400

    reduces transition duration

--- src/app/components/avatar.tsx
+++ src/app/components/avatar.tsx
@@ -1,7 +1,7 @@
 export default function Avatar({ color }: { color: string }) {
   return (
     <div
-      className="flex items-center justify-center p-6 sm:p-24 border-4 border-white transition-colors duration-700"
+      className="flex items-center justify-center p-6 sm:p-24 border-4 border-white transition-colors duration-300 ease-in-out rounded-full"
       style={{ backgroundColor: color }}
     >
       <span className="inline-block w-16 h-16 sm:w-32 sm:h-32 md:w-64 md:h-64 bg-white border-4 border-black rounded-full" />
